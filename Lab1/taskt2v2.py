1e9b7cb5024dba16837a95de4754e2e147c768d1
Andre Lim <Andre_Lim99@outlook.com>
2022-02-23 17:54:12 +0800

    Working Task2V2 file

--- Lab1/taskt2v2.py
+++ Lab1/taskt2v2.py
@@ -1,6 +1,6 @@
 import json
 
-from numpy import short
+import heapq
 
 # Declare s & t
 START = 1
@@ -12,133 +12,103 @@ I = 0
 # 287932
 
 
-def findShortestPath(adjList, eCost, dist, start, destination, removedEdges = []): # Using UCS
-    energyCost = 0
-    pi = []
-    pathCosts = []
-    frontier = []
+def UCS(adjList, eCost, dist, start, destination): # Using UCS
+    # Create Trackers
+    pq = []
+    # energyDict = {(start,0): 0}
+    minCost = {}
+    minDist = {}
     visited = []
+    parent = {}
 
-    # initialise arrays
-    for i in range(NUMNODES):
-        pi.append(-1)
-        visited.append(0)
-        pathCosts.append(float('inf'))
+    heapq.heappush(pq, (0, (start, 0))) # Push (distancec, (node, energyCost))
 
-    frontier.append(start)
-    pathCosts[start-1] = 0
+    # Explore Queue
+    while len(pq) != 0:
+        curDist, (curNode, curCost) = heapq.heappop(pq)
+        # print(f"Now visiting {curNode}, {curCost}.")
+    # Do not explore node if it's current distance to this node AND energy to this node are both > previous minimum
+        if curNode in minDist and minDist[curNode] <= curDist and curNode in minCost and minCost[curNode] <= curCost:
+            continue
+    # Update new min values
+        try:
+            if curDist < minDist[curNode]:
+                minDist[curNode] = curDist
+        except:
+            minDist[curNode] = curDist 
 
+        try:
+            if curCost < minCost[curNode]:
+                minCost[curNode] = curCost
+        except:
+            minCost[curNode] = curCost
+    # If destination found
+        if curNode == destination:
+            # print(parent[curNode, curCost, curDist])
+            return parent, curCost, curDist
 
-    while len(frontier) != 0:
-        # Get node with shortest distance in frontier
-        min = float('inf')
-        for el in frontier:
-            if pathCosts[el-1] < min:
-                min = pathCosts[el-1]
-                currentNode = el
-        # print(frontier.index(currentNode))
-        frontier.remove(currentNode)
+        visited.append((curNode, curCost))
 
-        # mark currentNode as visited
-        visited[currentNode-1] = 1
-        
-        if currentNode != start:
-            energyCost = energyCost + eCost[str(pi[currentNode-1])+","+str(currentNode)]
+    # Explore all neighbour nodes
+        for neighbour in adjList[str(curNode)]:
+            neighbour = int(neighbour)
+            newCost = curCost + eCost[str(curNode)+","+str(neighbour)]
+            newDist = curDist + dist[str(curNode)+","+str(neighbour)]
 
-        # Check if Destination Node
-        if currentNode == destination:
-            cur = destination
-            pre = {}
-            while cur != start:
-                pre[cur] = pi[cur-1]
-                cur = pi[cur-1]
-            return {"path": pre, "distance": pathCosts[destination-1], "energy": energyCost}
+    # Find energy cost to visit this node and only add if < budget
+            if newCost <= BUDGET:
+    # Check if node has ever been visited at this energyCost
+                if (neighbour, newCost) not in visited:
+                    # Update arrays
+                    parent[(neighbour, newCost, newDist)] = (curNode, curCost, curDist)
+                    # energyDict[(neighbour, newCost)]
+                    heapq.heappush(pq, (newDist, (neighbour, newCost)))
 
-
-        
-        # For adjacent nodes
-        for neighbour in adjList[str(currentNode)]:
-            # Check if this is the link that has been removed
-            if  (currentNode,int(neighbour)) in removedEdges:
-                continue        
-            elif visited[int(neighbour) - 1] == 0:
-                if pathCosts[int(neighbour)-1] > int(dist[str(currentNode) + "," + str(neighbour)]) + pathCosts[currentNode-1]:
-                    pathCosts[int(neighbour)-1] = int(dist[str(currentNode) + "," + str(neighbour)]) + pathCosts[currentNode-1]
-                    pi[int(neighbour)-1] = currentNode
-                    frontier.append(int(neighbour))
-
-    return {"path": None, "distance": -1, "energy": -1}
-
-
-def iterativeShortestPath(adjList, eCost, dist, start, destination): # Using UCS
-    shortestPaths = {1:findShortestPath(adjList, eCost, dist, start, destination)}
-    curEnergy = shortestPaths[1]["energy"]
-    removedEdges = []
-    
-    print(shortestPaths)
-    i = 0
-    k = 1
-
-    while shortestPaths[k]["distance"] < BUDGET:
-        # Find next shortest path
-        min = float('inf')
-        for key in shortestPaths[k]["path"]:
-            # print(f"Key: {key}")
-            # Check if this new path is next shortest
-            tmpRemovedEdges = removedEdges + [(shortestPaths[k]["path"][key],key)]
-            potentialShortest = findShortestPath(adjList, eCost, dist, start, destination, tmpRemovedEdges)
-            if potentialShortest["distance"] < min and potentialShortest["distance"] != -1:
-                shortestPaths[k+1] = potentialShortest
-                min = potentialShortest["distance"]
-                removedFrom = shortestPaths[k]["path"][key]
-                removedTo = key
-
-        
-        # Update k, curEnergy, removedEdges
-        removedEdges.append((removedFrom, removedTo))        
-        print(f"Removed Edges: {removedEdges}")
-        k = k+1
-        curEnergy = shortestPaths[k]["energy"]
-        print(f"Minimum {k}th: ", end='')
-        print(shortestPaths[k]["path"])
-        print(shortestPaths[k]["distance"])
-        print(shortestPaths[k]["energy"])
-
-
-    
-
-    return shortestPaths[k]
-
+    return None, None, None
 
 
 # Load Relevant Files
-with open("Dist.json") as f1, open("G.json") as f2, open("Cost.json")  as f3:
+with open("CZ3005-AI\Lab1\Dist.json") as f1, open("CZ3005-AI\Lab1\G.json") as f2, open("CZ3005-AI\Lab1\Cost.json")  as f3:
         distDict = json.load(f1)
         G = json.load(f2)
         cost = json.load(f3)
 
-
+# print(distDict)
 
 # print(len(gList))
 
 # Initialise all arrays
-# Currently swapped position of distance and energy json in parameters
-iterativeShortestPath(G, distDict, cost, 1, 50)
+parent, energy, distance =  UCS(G, cost, distDict, 1, 50)
+
+
+if parent is None:
+    print("Path not found")
+
+else:
+
+    cur = parent[DESTINATION, energy, distance]
+
+    path = [DESTINATION]
+    while cur[0] != START:
+        path.insert(0, cur[0])
+        cur = parent[cur]
+        
+
+    path.insert(0, START)
+
+    pathEnergy = 0
+    pathDistance = 0
+    for i in range(len(path)):
+        print(path[i], end='->')
+        # pathEnergy += cost[str(path[i]) + "," + str(path[i+1])]
+        # pathDistance += distDict[str(path[i]) + "," + str(path[i+1])]
 
 
 
+    print()
+    # print(f"Path Energy Cost: {pathEnergy}")
+    # print(f"Path Total Distance: {pathDistance}")
 
-
-# iterate through paths to find energy and distance
-# print(paths)
-# print("Shortest Path: ", end='')
-# for node in paths[0][0]:
-#     if node != DESTINATION:
-#         print(str(node)+"->", end='')
-#     else:
-#         print(node)
-# print(f"Shortest distance: {paths[0][2]} ")
-# print(f"Total Energy Cost: {paths[0][1]}")
-
-   
+    print(f"Energy Cost: {energy}")
+    print(f"Total Distance: {distance}")
 
